56ad432f3030db6fc4352a435a1e5a24d9d0c14a
amitchougale <auchougale333@gmail.com>
2023-01-10 17:20:18 +0530

    first commit

---
+++ src/com/string/StringDemo.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class StringDemo {
+	
+	public static void main(String[] args) {
+		
+		String s1 = "Amit";
+		String s2 = "Chougale";
+		
+		System.out.println(s1);
+	}
+
+}
